7efe878e7c4627b2ef0b6a557281df3b9e776a6c
chandan2107 <chandunaik2107@gmail.com>
2026-04-13 20:34:20 +0530

    Final commit

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import { useNavigate, Link } from 'react-router'
-import "../auth.form.scss"
+
 import { useAuth } from '../hooks/useAuth'
 import Loader from '../../interview/components/Loader'
 
