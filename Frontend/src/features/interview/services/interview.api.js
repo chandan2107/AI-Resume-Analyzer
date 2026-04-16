dc46ced33fbcae384eed1eed5c56a004cfd768ae
Chandan Naik <chandunaik2107@gmail.com>
2026-04-16 12:27:59 +0530

    final commit

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-resume-analyzer-backend-9vro.onrender.com",
     withCredentials: true,
 })
 
